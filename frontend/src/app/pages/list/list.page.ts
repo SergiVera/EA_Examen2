85f41b7f05f354580ff1a12acfa1937a2f84bdf2
SergiVera <sergitus1998@gmail.com>
2019-05-27 11:44:23 +0200

    all implemented

--- frontend/src/app/pages/list/list.page.ts
+++ frontend/src/app/pages/list/list.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Station} from '../../models/station';
+import {Bike} from '../../models/bike';
+import {ActivatedRoute} from '@angular/router';
+import {StationService} from '../../services/station.service';
+import {BikeService} from '../../services/bike.service';
+import {AlertController} from "@ionic/angular";
 
 @Component({
   selector: 'app-list',
@@ -6,34 +12,88 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['list.page.scss']
 })
 export class ListPage implements OnInit {
-  private selectedItem: any;
-  private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
-  ];
-  public items: Array<{ title: string; note: string; icon: string }> = [];
-  constructor() {
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+  stationBikeDetail: Station;
+  unassignedBikes: Bike[];
+  body: object;
+
+  constructor(private activatedRouter: ActivatedRoute, private stationService: StationService, private bikeService: BikeService,
+              private alertController: AlertController) {
+    this.stationBikeDetail = new Station();
+    this.unassignedBikes = [];
   }
 
   ngOnInit() {
+    this.activatedRouter.params.subscribe(params => {
+      if (typeof params.stationId !== 'undefined') {
+        this.stationBikeDetail._id = params.stationId;
+      } else {
+        this.stationBikeDetail._id = '';
+      }
+    });
+    this.getBikeDetail(this.stationBikeDetail._id);
+    this.getUnassignedBikes();
   }
-  // add back when alpha.4 is out
-  // navigate(item) {
-  //   this.router.navigate(['/list', JSON.stringify(item)]);
-  // }
+
+  async presentAlert(id: string, i: number) {
+    const alert = await this.alertController.create({
+      subHeader: 'Be careful!',
+      message: 'Are you sure you want to delete this bike?',
+      buttons: [{
+        text: 'Cancel'
+      }, {
+        text: 'Delete',
+        handler: () => {
+          this.deleteBikeStation(id, i);
+        }
+      }]
+    });
+
+    await alert.present();
+  }
+
+  async getUnassignedBikes() {
+    await this.bikeService.getUnassignedBikes()
+        .subscribe(res => {
+          console.log(res);
+          this.unassignedBikes = res as Bike[];
+        });
+    console.log(this.unassignedBikes);
+  }
+
+  async getBikeDetail(id: string) {
+    await this.stationService.getStationBikeDetail(id)
+        .subscribe(res => {
+          console.log(res);
+          this.stationBikeDetail = res as Station;
+        });
+    console.log(this.stationBikeDetail);
+  }
+
+  async deleteBikeStation(id: string, i: number) {
+    await this.stationService.deleteBikeStation(this.stationBikeDetail._id, id)
+          .subscribe(res => {
+                console.log(res);
+                this.stationBikeDetail.bikes.splice(i, 1);
+                this.getUnassignedBikes();
+              },
+              err => {
+                console.log(err);
+              });
+  }
+
+  async addBikeStation(id: string, i: number) {
+    this.body = {
+      stationId: this.stationBikeDetail._id,
+      bikeId: id
+    };
+    await this.stationService.postBikeStation(this.body)
+        .subscribe(res => {
+              console.log(res);
+              this.unassignedBikes.splice(i, 1);
+              this.getBikeDetail(this.stationBikeDetail._id);
+            },
+            err => {
+              console.log(err);
+            });
+  }
 }
